373378e606173693ecfa1915154126a6236ea6d1
IsItSis? <36586296+rookieman88@users.noreply.github.com>
2020-05-06 12:21:48 +0900

    Ready for update V2

--- settings.js
+++
@@ -1,3 +0,0 @@
-exports.TOKEN = '' // 여기에 토큰을 적어주세요 (https://discordapp.com/developers/applications/ 에서 발급 가능)
-exports.PREFIX = '' // 여기에 접두사를 적어주세요 (예: '!')
-exports.OWNERS = '' // 여기에 관리자 ID 를 적어주세요 (예: '417571990820618250, 523282229397422081')
